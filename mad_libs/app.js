f636e0d32676ca4a9626fe00639dae5dbc5fad16
rohansinha01 <rohansinha01@gmail.com>
2023-11-19 17:12:00 -0500

    madlib added

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -7,7 +7,12 @@ const words = {
 }
 
 const startMadlib = () => {
-  let age = prompt("Give me a number")
-   return words.number.push()
+  words.number = prompt('Give me a number')
+  words.adjective = prompt('Give me an adjective')
+  words.pluralNoun = prompt('Give me a pluraled Noun')
+  words.adverb = prompt('Give me an adverb')
+  words.anotherAdjective = prompt('Give me another adjective')
+  alert(`Once upon a time a group of ${words.number} General Assembly graduates got together and made a startup called ${words.adjective} Technologies.
+  Their goal was to create smart ${words.pluralNoun}. They approached the challenge ${words.adverb} which ultimately lead them to ${words.anotherAdjective} fame.`)
 } 
 startMadlib()
